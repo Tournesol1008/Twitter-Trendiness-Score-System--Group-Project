c05b0d24530c34c94e9fce78d3d4cf013a481947
rsheth313 <90640048+rsheth313@users.noreply.github.com>
2021-11-15 12:19:30 -0600

    Add files via upload
    To halt the stream manually, press ctrl-c in the VM. Then do 'python3 server.py > unfiltered.txt' in the VM to write results.

---
+++ server.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 16:53:14 2021
+
+@author: Romeel
+"""
+
+import requests
+import os
+import json
+import pandas as pd
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAPvIUgEAAAAAYT52qKny6B4Kg%2FcLZxGDMP%2FCQ%2BM%3DaUQwwWeeBgOAgfRtNs36d0ymOLCwtVhzgdr1skzZX1Ge3ANYYu"
+
+def create_url():
+    return "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at,lang"
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2SampledStreamPython"
+    return r
+results = []
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth, stream=True)
+    print(response.status_code)
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            results.append(print(json.dumps(json_response, indent=4, sort_keys=True)))
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text, 
+            )
+        )
+
+
+def main():
+    url = create_url()
+    timeout = 0
+    while True:
+        connect_to_endpoint(url)
+        timeout += 1
+
+
+if __name__ == "__main__":
+    main()
