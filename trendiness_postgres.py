9651b53f419e40f21433c0b0073b301f3cdec9c0
Weiyuan Liang <wliang44@wisc.edu>
2021-11-24 16:55:26 -0600

    commets changes

--- trendiness_postgres.py
+++ trendiness_postgres.py
@@ -1,35 +1,4 @@
-
-"""from string import punctuation
-
-def process_file(): 
-    try: # open file and handle errors
-        f = open("tweets.txt", "r",encoding='utf-8')
-    except IOError as s:
-        print(s)
-        return None
-    try: # read file and handle errors
-        readtws = f.read()
-    except:
-        print("Read File Error!")
-        return None
-    f.close()  
-    return readtws
-    
-#2.Processing the file，count the frequency of each words，and store them in word_freq
-def process_tweets(readtws):
-    if readtws:
-        word_freq = {}
-        for i in '!"#$%&()*+-,-./:;<=>?@“”[\\]^_{|}~':
-            readtws = readtws.replace(i, " ") # replace special characters
-            readtws = readtws.lower() # convert uppercase to lowercase
-            words = readtws.split() # split string
-        for word in words:
-            word_freq[word] = word_freq.get(word, 0) + 1
-        return word_freq
-"""
-#Read word_count_postgres.py.py result
-
-
+#Read Word Input
 
 import argparse
 parser = argparse.ArgumentParser(description='Count word frequency')
