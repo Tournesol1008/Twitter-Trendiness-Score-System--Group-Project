e5aad300549e3ecd607d19d8f127935947662fc1
Dan He <dhe36@wisc.edu>
2021-11-24 00:40:28 -0600

    this is initial idea of the schema and trendiness table

--- schema_postgres.sql
+++ schema_postgres.sql
@@ -1,32 +1,8 @@
-CREATE OR REPLACE TABLE t (obj jsonb);
-
-insert into t (obj)
-select
-    regexp_split_to_table(
-        replace(v, $$"}{"$$, $$"}djue748wBc,l;09{"$$),
-        'djue748wBc,l;09'
-    )::jsonb
-from (
-
-) s;
-
-COPY t from '///home/gb760/Group5-760/tweet_json_file.json';
- select values->>'created_at' as CreationDate,
-       values->>'id' as userid,
-       values->>'lang' as lang,
-       values->>'text' as text     
-  from(
-          select json_array_elements(values) as values 
-          from tweet_import
-  ) a;
-
-
-
-CREATE ORc REPLACE TABLE Tweets(
+CREATE SCHEMA milestone2
+SET search_path TO 'milestone2';
+CREATE TABLE IF NOT EXISTS Trendiness(
    CreationDate timestamp NOT NULL,
-   userid integer NOT NULL,
-   lang varchar(255) NOT NULL,
-   text integer NOT NULL,
-   CONSTRAINT id_pk PRIMARY KEY (userid)
-);
+   Word varchar(255) NOT NULL,
+   WordNum integer NOT NULL,
+   TrendinessScore float NOT NULL);
 
