9a184d197236cea2c50591ed0790693f72c47e10
Dan He <dhe36@wisc.edu>
2021-11-23 18:47:47 -0600

    inital schema design idea using sql read tweets from json file

--- schema_postgres.sql
+++ schema_postgres.sql
@@ -1,5 +1,32 @@
-CREATE TABLE TweetWord(
-    Time timestamp 		not null,
-    Word varchar(255)  	not null,
-    WordCount int		not null,
-    TrendinessScore float	not null);
+CREATE OR REPLACE TABLE t (obj jsonb);
+
+insert into t (obj)
+select
+    regexp_split_to_table(
+        replace(v, $$"}{"$$, $$"}djue748wBc,l;09{"$$),
+        'djue748wBc,l;09'
+    )::jsonb
+from (
+
+) s;
+
+COPY t from '///home/gb760/Group5-760/tweet_json_file.json';
+ select values->>'created_at' as CreationDate,
+       values->>'id' as userid,
+       values->>'lang' as lang,
+       values->>'text' as text     
+  from(
+          select json_array_elements(values) as values 
+          from tweet_import
+  ) a;
+
+
+
+CREATE ORc REPLACE TABLE Tweets(
+   CreationDate timestamp NOT NULL,
+   userid integer NOT NULL,
+   lang varchar(255) NOT NULL,
+   text integer NOT NULL,
+   CONSTRAINT id_pk PRIMARY KEY (userid)
+);
+
