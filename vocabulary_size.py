732e82bc5b94e6116dea78509d8ecc671a357907
Tournesol1008 <mchen389@wisc.edu>
2021-11-26 22:25:34 -0600

    Count unique word of all tweets in tweets.txt.

---
+++ vocabulary_size.py
@@ -0,0 +1,8 @@
+with open("tweets.txt", "r") as file:
+    lines = file.read().splitlines()
+
+    unique_word = set()
+    for line in lines:
+        unique_word |= set(line.split())
+
+    print(f"Unique words: {len(unique_word)}")
