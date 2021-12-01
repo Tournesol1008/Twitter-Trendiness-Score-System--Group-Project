1dd01cd84d97bff0c8b6348e6be17fdfcc2490e6
Dan He <dhe36@wisc.edu>
2021-11-30 19:24:35 -0600

    change current time calculation to the last data date

--- word_count_postgres.py
+++ word_count_postgres.py
@@ -4,18 +4,20 @@ import psycopg2
 import pandas as pd
 
 #creating data table
-conn = psycopg2.connect("dbname=tweets_info user=gb760")
+conn = psycopg2.connect("dbname=gb760 user=gb760")
 cur = conn.cursor()
 conn.commit()
 
-cur.execute("SELECT * FROM Tweets_time_and_text")
+cur.execute("SELECT * FROM Tweets_Table")
 record = cur.fetchall()
 df = pd.DataFrame(record)
-cur_min = df.iloc[0,2]
-cur_sec = df.iloc[0,3]
+cur_date = df.iloc[-1,0]
+cur_hour = df.iloc[-1,1]
+cur_min = df.iloc[-1,2]
+cur_sec = df.iloc[-1,3]
 row_list = []
 for i in range(len(df)):
-    if df.iloc[i,2] == cur_min and df.iloc[i,3] <= 59:
+    if df.iloc[i,0] == cur_date and df.iloc[i,1] == cur_hour and df.iloc[i,2] == cur_min and df.iloc[i,3] <= 59:
         new_line = df.iloc[i]
         row_list.append(new_line)
     else:
@@ -31,6 +33,7 @@ def process_tweets(new_df,phrase):
         text = text.lower() # convert uppercase to lowercase
     count = text.count(phrase)       
     return count
+print(new_df)
         
 import argparse
 parser = argparse.ArgumentParser(description='Count word frequency')
