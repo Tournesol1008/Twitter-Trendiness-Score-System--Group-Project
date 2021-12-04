4c30338c8399cb07a87c58e0dd2ea595f7ec2f31
Nef Jin <njin23@wisc.edu>
2021-12-03 23:44:24 -0600

    change file producer.py

--- producer.py
+++ producer.py
@@ -1,22 +1,18 @@
+from time import sleep
+from json import dumps
 from kafka import KafkaProducer
-import json
-import os
-import time
-from sys import argv
- 
-producer = KafkaProducer(bootstrap_servers='192.168.120.11:9092')
- 
-def log(str):
-    t = time.strftime(r"%Y-%m-%d_%H-%M-%S",time.localtime())
-    print("[%s]%s"%(t,str))
- 
-def list_file(path):
-    dir_list = os.listdir(path)#grabbing and saving as .json format, then send them one by one. # I will adjust it later if we use data from .json
-    for f in dir_list:
-         producer.send('world',f)
-         producer.flush()
-         log('send: %s' % (f))
- 
-list_file(argv[1])
-producer.close()
-log('done')
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:  
+                         dumps(x).encode('utf-8'))
+
+#for e in range(1000):
+    #data = {'number' : e}
+   # producer.send('gb760', value=data)
+
+import yaml
+with open('tweet_json_file.json') as f:
+    for line in f:
+        d = yaml.safe_load(line)
+        jd = json.dumps(d)
+        producer.send_messages(b'gb760',jd)
