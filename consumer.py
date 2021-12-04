dc11d0f3295917c980f95eef121f9b7c9151cc63
Nef Jin <njin23@wisc.edu>
2021-12-04 00:45:23 -0600

    file consumer.py

--- consumer.py
+++ consumer.py
@@ -1,21 +1,45 @@
 
- 
 from kafka import KafkaConsumer
-import time
- 
-def log(str):
-        t = time.strftime(r"%Y-%m-%d_%H-%M-%S",time.localtime())
-        print("[%s]%s"%(t,str))
- 
-log('start consumer')
-#consuming "world" from "192.168.120.11:9092" -- the Topic; "consumer group" is "consumer-20171017"
+from json import loads
 
-consumer=KafkaConsumer('world',group_id='consumer-20171017',bootstrap_servers=['192.168.120.11:9092'])
-for msg in consumer:
-        recv = "%s:%d:%d: key=%s value=%s" %(msg.topic,msg.partition,msg.offset,msg.key,msg.value)#getting info from consumer which can be input into database directly
-        log(recv)
+consumer = KafkaConsumer(
+    'gb760',
+     bootstrap_servers=['localhost:9092'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: x.decode('utf-8'))
+
+#for message in consumer:
+    #print(message)
+    
+
+
+def process_data():        
         
+    for message in consumer:
         
+        content = json.loads(mseeage) #"load" -- converting to json  #data receiving from producer
+        date = content['created_at'].split('T')[0]  #processing data to get CreationDate from json
+        hour = content['created_at'].split('T')[1].split(':')[0] #processing data to get CreationHour from json
+        min = content['created_at'].split('T')[1].split(':')[1]  #processing data to get CreationMinute from json
+        sec = content['created_at'].split('T')[1].split(':')[2]  #processing data to get CreationSeconds from json
         
+    return date,hour,min,sec,text  #processing data to get text from json
+    
+def send_to_db():
+
+    print ("succed!")#待补充
+    
+if __name__ == '__main__'：
+
+    creatDate,creatHour,creatMin,creatSec,text = process_data()
+    send_to_db()
+
+
+
+
+
+#Lack of datebase conf info 
 
 
